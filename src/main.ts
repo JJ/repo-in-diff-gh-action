f315ace8572213d0231ebe900ea0f9d0abe12583
JJ Merelo <jjmerelo@gmail.com>
2019-09-09 12:16:09 +0200

    Checks for null and errors refs #24

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import * as core from '@actions/core';
+const {GitHub, context} = require('@actions/github')
 
 async function run() {
   try {
       const diff = process.env.diff as string;
-      const ghRepo = /github.com\/(\w+)\/(\w+)/.exec(diff)
-      console.log( diff )
+      var ghRepoMatch = /github.com\/(\w+)\/(\w+)/.exec(diff)
+      if  ( ghRepoMatch === null ) {
+	  core.setFailed("There's no repo in this diff")
+      } else {
+	  const user = ghRepoMatch[1]
+	  const repo = ghRepoMatch[2]
+	  const token = core.getInput('github-token', {required: true})
+	  const github = new GitHub(token, {} )
+	  const ghRepo = github.repos.get( user, repo )
+	  console.log(ghRepo)
+      }
   } catch (error) {
     core.setFailed(error.message);
   }
