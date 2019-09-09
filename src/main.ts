e01a0d67bfad27aa30b9ccbc6e1468e719833b15
JJ Merelo <jjmerelo@gmail.com>
2019-09-09 09:42:51 +0200

    Extracts regex

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core';
 async function run() {
   try {
       const diff = core.getInput('diff');
-      const ghRepo = diff.match(/github.com/(\w+)\/(\w+)/)
+      const ghRepo = /github.com\/(\w+)\/(\w+)/.exec(diff)
       console.log( ghRepo )
   } catch (error) {
     core.setFailed(error.message);
