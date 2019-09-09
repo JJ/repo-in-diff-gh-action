45735c1bbb568f217868ef2310e3b1716eb6cc9d
JJ Merelo <jjmerelo@gmail.com>
2019-09-09 08:41:12 +0200

    Adds first version

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const diff = core.getInput('diff');
+    core.debug(`Got ${diff}`);
   } catch (error) {
     core.setFailed(error.message);
   }
