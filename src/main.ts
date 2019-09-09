aa30f77a470b1be4d1e70e5d425cebce4f715b29
JJ Merelo <jjmerelo@gmail.com>
2019-09-09 11:34:15 +0200

    Right name

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-      const diff = process.env.DIFF as string;
+      const diff = process.env.diff as string;
       const ghRepo = /github.com\/(\w+)\/(\w+)/.exec(diff)
       console.log( diff )
   } catch (error) {
